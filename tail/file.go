1b84dfe0f90ff6b5c4b4364372b7cf7add766f38
yulianedyalkova <julianedialkova@gmail.com>
2021-11-01 12:37:17 +0000

    Do not restart on inode change

--- tail/file.go
+++ tail/file.go
@@ -14,61 +14,66 @@ func File(name string, logger micrologger.Logger) (<-chan string, error) {
 	ctx := context.Background()
 	output := make(chan string, 2)
 
+	go func() {
+		defer close(output)
+		for {
+			if err := readFile(ctx, name, output, logger); err != nil {
+				logger.Errorf(ctx, err, "read")
+				break
+			}
+		}
+	}()
+
+	return output, nil
+}
+
+func readFile(ctx context.Context, name string, output chan string, logger micrologger.Logger) error {
 	f, err := os.Open(name)
 	if err != nil {
-		logger.Errorf(ctx, err, "open")
-		return nil, microerror.Mask(err)
+		return err
 	}
+	defer f.Close()
 
 	// Seek to the end of the file to not repeat earlier lines.
 	_, err = f.Seek(0, 2)
 	if err != nil {
-		return nil, microerror.Mask(err)
+		return err
 	}
 
 	// Capture file inode number to detect log rotation.
 	inodeBeginning, err := getInode(name)
 	if err != nil {
-		logger.Errorf(ctx, err, "stat")
-		return nil, microerror.Mask(err)
+		return err
 	}
 
 	reader := newLineReader(f)
 
-	go func() {
-		defer f.Close()
-		defer close(output)
+	err = reader.readLines(output)
+	if err != nil {
+		return err
+	}
+
+	for {
+		inode, err := getInode(name)
+		if err != nil {
+			return err
+		}
+
+		if inodeBeginning != inode {
+			// File has been rotated. We need to restart.
+			logger.Debugf(ctx, "inode has changed (was %q, now %q)", inodeBeginning, inode)
+			break
+		}
 
 		err = reader.readLines(output)
 		if err != nil {
-			logger.Errorf(ctx, err, "read")
-			return
+			return err
 		}
 
-		for {
-			inode, err := getInode(name)
-			if err != nil {
-				logger.Errorf(ctx, err, "stat")
-				break
-			}
+		time.Sleep(10 * time.Millisecond)
+	}
 
-			if inodeBeginning != inode {
-				// File has been rotated. We need to restart.
-				logger.Debugf(ctx, "inode has changed (was %q, now %q)", inodeBeginning, inode)
-				break
-			}
-
-			err = reader.readLines(output)
-			if err != nil {
-				logger.Errorf(ctx, err, "read")
-				break
-			}
-
-			time.Sleep(10 * time.Millisecond)
-		}
-	}()
-
-	return output, nil
+	return nil
 }
 
 func getInode(fileName string) (uint64, error) {
